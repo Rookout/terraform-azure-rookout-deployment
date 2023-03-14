eabce815a4156d38bfdf42160a3893f29119b283
Gosha <gosha@rookout.com>
2023-03-14 18:51:15 +0200

    feat: added support for pre-existing subnets

---
+++ examples/private_existing_vnet_and_subnets.tf
@@ -0,0 +1,14 @@
+module "rookout" {
+    source  = "Rookout/rookout-deployment/azure"
+    # version = x.y.z
+    
+    rookout_token = "YOUR_TOKEN"
+    internal = true
+
+    create_vnet = false
+    existing_vnet_name = "..."
+    existing_vnet_resource_group_name = "..."
+
+    subnet_app_serivce_name = "..."
+    private_endpoint_subnet_name = "..."
+}
